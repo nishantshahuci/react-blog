51b837bca8e3d796793097c2c4f703d19961d2f3
Nishant Shah <nishantshahuci@gmail.com>
2019-02-19 10:59:47 -0500

    Add reducer to update state with users and show users in UserHeader component

---
+++ src/reducers/usersReducer.js
@@ -0,0 +1,9 @@
+// users reducer
+export default (state = [], action) => {
+    switch(action.type) {
+        case 'FETCH_USER':
+            return [...state, action.payload];
+        default:
+            return state;
+    }
+}
