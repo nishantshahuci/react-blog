6afb0298187f625c9955edb0923d6f7213f81c15
Nishant Shah <nishantshahuci@gmail.com>
2019-02-19 10:49:26 -0500

    Add UserHeader component

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,7 +11,7 @@ export const fetchPosts = () => async dispatch => {
 }
 
 export const fetchUser = id => async dispatch => {
-    const respones = await jsonPlaceholder.get(`/users/${id}`);
+    const response = await jsonPlaceholder.get(`/users/${id}`);
     dispatch({
         type: 'FETCH_USER',
         payload: response.data
