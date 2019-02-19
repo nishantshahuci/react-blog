2e6e0b58c2829dcd4cac6008370dbd03f7427d4d
Nishant Shah <nishantshahuci@gmail.com>
2019-02-19 10:43:18 -0500

    Add action to fetch a user

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,12 +2,18 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 // action creators
 
-export const fetchPosts = () => {
-    return async dispatch => {
-        const response = await jsonPlaceholder.get('/posts');
-        dispatch({
-            type: 'FETCH_POSTS',
-            payload: response.data
-        });
-    }
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+    dispatch({
+        type: 'FETCH_POSTS',
+        payload: response.data
+    });
+}
+
+export const fetchUser = id => async dispatch => {
+    const respones = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({
+        type: 'FETCH_USER',
+        payload: response.data
+    });
 }
