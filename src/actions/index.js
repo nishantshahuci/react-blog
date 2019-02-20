a2a41ceba846e9a76d26da71aab00f41204a96fb
Nishant Shah <nishantshahuci@gmail.com>
2019-02-20 13:20:38 -0500

    Final comments

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,6 +7,13 @@ export const fetchPostsAndUsers = () => async (dispatch, getState) => {
     await dispatch(fetchPosts());
     const userIds = _.uniq(_.map(getState().posts, 'userId'));
     userIds.forEach(id => dispatch(fetchUser(id)));
+
+    // // uses chain from lodash
+    // _.chain(getState().posts)
+    // .map('userId')
+    // .uniq()
+    // .forEach(id => dispatch(fetchUser(id)));
+    // .value();
 }
 
 export const fetchPosts = () => async dispatch => {
